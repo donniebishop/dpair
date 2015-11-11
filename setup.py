64598b2e3dbab4eb093bbc1c7d7338f2c05d0937
Donnie Bishop <donnie.a.bishop@gmail.com>
2015-11-11 07:17:52 -0500

    Added MANIFEST.in; crossing fingers for setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from distutils.core import setup
 
 setup(name='dpair',
-      version='0.1.0',
-      packages=['dpair'],
-      author='Donnie Bishop',
-      author_email='donnie.a.bishop@gmail.com',
-      license='Apache 2.0',
-      data_files=[('assets', ['google-10000-english.txt'])]
-      )
+    version='0.1.0',
+    packages=['dpair'],
+    author='Donnie Bishop',
+    author_email='donnie.a.bishop@gmail.com',
+    license='Apache 2.0',
+    )
