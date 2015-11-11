6165064d39dd9c091e572ccecfceae43caffe5b1
Donnie Bishop <donnie.a.bishop@gmail.com>
2015-11-11 07:52:07 -0500

    Setup.py should be good now.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 setup(name='dpair',
     version='0.1.0',
-    packages=['dpair'],
     author='Donnie Bishop',
     author_email='donnie.a.bishop@gmail.com',
     license='Apache 2.0',
+    packages=['dpair'],
+    package_data={'dpair': ['assets/*.txt']},
     )
