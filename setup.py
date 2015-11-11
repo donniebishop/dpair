cc128066f44aed0b03df294b42f0001b7e508a26
Donnie Bishop <donnie.a.bishop@gmail.com>
2015-11-11 04:01:52 -0500

    Forgot some commas..

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='dpair',
       version='0.1.0',
       py_modules=['dpair'],
-      author='Donnie Bishop'
+      author='Donnie Bishop',
       author_email='donnie.a.bishop@gmail.com',
-      license='Apache 2.0'
+      license='Apache 2.0',
       )
