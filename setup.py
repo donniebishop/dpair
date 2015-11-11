602e1338cf1201e0675cd47328ff9a287dcdc2dc
Donnie Bishop <donnie.a.bishop@gmail.com>
2015-11-11 07:54:32 -0500

    Setup.py should be good. Improve wordlist import

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(name='dpair',
     author='Donnie Bishop',
     author_email='donnie.a.bishop@gmail.com',
     license='Apache 2.0',
+    packages=['dpair'],
+    package_data={'dpair': ['assets/*.txt']},
     )
