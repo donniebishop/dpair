6dd6183885ef8dcde59ab9a2295d6a3f288310b5
Donnie Bishop <donnie.a.bishop@gmail.com>
2015-11-11 03:55:39 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='dpair',
+      version='0.1.0',
+      py_modules=['dpair'],
+      author='Donnie Bishop'
+      author_email='donnie.a.bishop@gmail.com',
+      license='Apache 2.0'
+      )
