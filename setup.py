a0a49945254b5417223230b9b16f7b90d3529734
Donnie Bishop <donnie.a.bishop@gmail.com>
2015-11-11 04:07:14 -0500

    Let's try to get this setup thing right...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='dpair',
       version='0.1.0',
-      py_modules=['dpair'],
+      packages=['dpair'],
       author='Donnie Bishop',
       author_email='donnie.a.bishop@gmail.com',
       license='Apache 2.0',
